2d5dae006dd8a4af780487227d539cf84a9c2849
tyn53 <jbush53@gmail.com>
2018-03-07 20:49:33 -0600

    added new empty classes

--- Models/Campaign.cs
+++ Models/Campaign.cs
@@ -1,4 +1,6 @@
-using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace CampaignDesignerBE.Models
 {
@@ -6,5 +8,7 @@ namespace CampaignDesignerBE.Models
   {
     public int CampaignID { get; set; }
     public string CampaignName { get; set; }
+
+    public IEnumerable<PlotPoint> {get;set;}
   }
 }
